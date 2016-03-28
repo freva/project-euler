6ebd49ba980248408ecf86b92799a9224d3ca05c
Valerij Fredriksen <valerijf@stud.ntnu.no>
2016-03-28 12:57:09 +0200

    Solved three problems without use of Collections

--- src/com/freva/projecteuler/lvl0/Problem023.java
+++ src/com/freva/projecteuler/lvl0/Problem023.java
@@ -2,11 +2,9 @@ package com.freva.projecteuler.lvl0;
 
 import com.freva.projecteuler.Problem;
 
-import java.util.ArrayList;
-
 //Euler problem #23:
 //Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
-//Answer: 4179871, Time: 18ms
+//Answer: 4179871, Time: 11ms
 
 public class Problem023 implements Problem {
     public Number solve() {
@@ -19,17 +17,18 @@ public class Problem023 implements Problem {
             }
         }
 
-        final ArrayList<Integer> abundantNumbers = new ArrayList<>(LIMIT / 3);
+        int numberOfAbundantNumbers = 0;
+        final int[] abundantNumbers = new int[LIMIT / 3];
         for (int m = 12; m < LIMIT; m++) {
             if (sumOfDivisors[m] > m) {
-                abundantNumbers.add(m);
+                abundantNumbers[numberOfAbundantNumbers++] = m;
             }
         }
 
         int sumNonAbundant = LIMIT / 2 * (LIMIT - 1);
         final boolean[] isAbundantNumber = new boolean[LIMIT];
-        for (int i = 0; i < abundantNumbers.size(); i++) {
-            for (int j = i, sum; (sum = abundantNumbers.get(i) + abundantNumbers.get(j)) < LIMIT; j++) {
+        for (int i = 0; i < numberOfAbundantNumbers; i++) {
+            for (int j = i, sum; (sum = abundantNumbers[i] + abundantNumbers[j]) < LIMIT; j++) {
                 if (!isAbundantNumber[sum]) {
                     isAbundantNumber[sum] = true;
                     sumNonAbundant -= sum;
--- src/com/freva/projecteuler/lvl0/Problem024.java
+++ src/com/freva/projecteuler/lvl0/Problem024.java
@@ -3,25 +3,30 @@ package com.freva.projecteuler.lvl0;
 import com.freva.projecteuler.Functions;
 import com.freva.projecteuler.Problem;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 //Euler problem #24:
 //What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 //Answer: 2783915460, Time: 0ms
 
 public class Problem024 implements Problem {
     public Number solve() {
-        final ArrayList<Integer> digits = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+        final int[] digits = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         int position = 999_999;
 
         long permutation = 0;
-        for (int n = digits.size(); n > 0; n--) {
+        for (int n = digits.length; n > 0; n--) {
             int step = Functions.TEN_FACTORIALS[n - 1];
             int r = position / step;
             position -= r * step;
 
-            permutation = 10 * permutation + digits.remove(r);
+            int rThDigit = -1;
+            while (r >= 0) { //Find r'th unused digit
+                if (digits[++rThDigit] != -1) {
+                    r--;
+                }
+            }
+
+            permutation = 10 * permutation + digits[rThDigit];
+            digits[rThDigit] = -1; //Mark digit as used
         }
 
         return permutation;
